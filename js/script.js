022514df5ce05f72511438aa698dbba9c78af017
Rubens Rodrigues <misterubens@gmail.com>
2023-01-18 10:04:38 -0300

    Atualizações

--- js/script.js
+++ js/script.js
@@ -62,28 +62,7 @@ function initNavBar() {
 }
 initNavBar();
 
-// Scroll
-const item = document.querySelectorAll("[data-anime");
-
-const animeScroll = () => {
-  const windowTop = window.pageYOffset + window.innerHeight * 0.9;
-
-  item.forEach((element) => {
-    if (windowTop > element.offsetTop) {
-      element.classList.add("animate");
-    } else {
-      element.classList.remove("animate");
-    }
-  });
-};
-
-animeScroll();
-
-window.addEventListener("scroll", () => {
-  animeScroll();
-});
-
-// scroll 2
+// scroll
 
 function initAnimacaoScroll() {
   const sections = document.querySelectorAll(".js-scroll");
